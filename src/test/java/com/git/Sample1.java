3e4498f0e767826dce7682f55444bab0c15b8931
shilpa-kasi <npshilpa2011@gmail.com>
2025-04-10 15:09:34 +0530

    hai i change first line

---
+++ src/test/java/com/git/Sample1.java
@@ -0,0 +1,10 @@
+package com.git;
+
+public class Sample1 
+{
+	public static void main(String[] args) {
+		System.out.println("hai world");
+		System.out .println("hello");
+	}
+
+}
---
+++ src/test/java/com/git/Sample2.java
@@ -0,0 +1,9 @@
+package com.git;
+
+public class Sample2
+{
+	public static void main(String[] args) {
+		System.out.println("Hello the world same high");
+	}
+
+}
